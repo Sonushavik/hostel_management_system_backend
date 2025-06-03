1274a786bca6478a82d4da7cfc68247a8eed2c38
Sonushavik <sonuk06212@gmail.com>
2025-06-03 23:01:22 +0530

    backend phase completed

--- router/admin-router.js
+++ router/admin-router.js
@@ -13,5 +13,4 @@ router.route("/user/:userId").delete(authMiddleware,adminMiddleware,deleteUser)
 router.route("/application/:applicationId").delete(authMiddleware,adminMiddleware,deleteApplication)
 router.route("/contact/:contactId").delete(authMiddleware,adminMiddleware,deleteContact)
 
-
 module.exports = router;
